3e8ea0e60bc611c3dfdd20a09c18512710f8016f
Dominik Halle <dominik.halle@schnapptack.de>
2017-12-21 14:55:45 +0100

    fixed admin urls include for new include mechanism

--- django_productline/features/djpladmin/admin_urls.py
+++ django_productline/features/djpladmin/admin_urls.py
@@ -5,4 +5,4 @@ def get_admin_urls():
     from django.conf.urls import include, url
     from django.contrib import admin
     from django.conf import settings
-    return [url(r'^%s' % settings.ADMIN_URL, include(admin.site.urls))]
+    return [url(r'^%s' % settings.ADMIN_URL, admin.site.urls)]
