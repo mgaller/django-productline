da86099661ad6818ba90408fd1bf1772b3b92a18
Dominik Halle <dominik.halle@schnapptack.de>
2018-03-27 11:29:24 +0200

    zipdir: prevent the generated_static path from being zipped

--- django_productline/utils.py
+++ django_productline/utils.py
@@ -22,21 +22,20 @@ def create_or_append_to_zip(file_handle, zip_path, arc_name=None):
 def zipdir(src_path, target_path, wrapdir=''):
     """
     Zips the pat
-    :param path: the path to the directory
-    :param ziph: the zipfile handle
     :param wrapdir: wrap all contents in an additional dir
     :return:
     """
-
     zipf = zipfile.ZipFile(target_path, 'w', zipfile.ZIP_DEFLATED)
 
     for root, dirs, files in os.walk(src_path):
-        for file in files:
-            path = os.path.join(root, file)
-            # get the relative path from the src_path in order to avoid an archive
-            # of absolute paths including your home directory.
-            rel_path = os.path.relpath(path, src_path)
-            zipf.write(path, os.path.join(wrapdir, rel_path))
+        # exclude generated_static-dir
+        if 'generated_static' not in root:
+            for file in files:
+                path = os.path.join(root, file)
+                # get the relative path from the src_path in order to avoid an archive
+                # of absolute paths including your home directory.
+                rel_path = os.path.relpath(path, src_path)
+                zipf.write(path, os.path.join(wrapdir, rel_path))
 
     zipf.close()
 
